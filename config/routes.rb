2a3ef7b66bfb4fb81dedf7651d46dbddf36b1f33
Hanoch Griner <eugriner@gmail.com>
2016-06-22 10:04:44 -0300

    ADD - Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,16 @@ Rails.application.routes.draw do
   #   end
   root to: 'application#angular'
 
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:show, :create] do
+      member do
+        put '/upvote' => 'comments#upvote'
+      end
+    end
+
+    member do
+      put '/upvote' => 'posts#upvote'
+    end
+  end
+
 end
